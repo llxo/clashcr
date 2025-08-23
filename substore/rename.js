3a47317a53e7623552cca215fca4c9a23f87bca7
llx <tv4hack@outlook.com>
2025-08-23 12:50:55 +0800

    更新脚本日期和示例链接，调整参数说明

--- substore/rename.js
+++ substore/rename.js
@@ -1,6 +1,7 @@
 /**
- * 更新日期：2024-04-05 15:30:15
+ * 更新日期：2025-08-23
  * 用法：Sub-Store 脚本操作添加
+ * 修改自：https://github.com/Keywos/rule/blob/main/rename.js
  * rename.js 以下是此脚本支持的参数，必须以 # 为开头多个参数使用"&"连接，参考上述地址为例使用参数。 禁用缓存url#noCache
  *
  *** 主要参数
@@ -27,13 +28,13 @@
  *
  *** 机场名称参数
  * [name=]  节点添加机场名称；
- * [nf]     把 name= 的值放在最前面
  * [ne]     把 name= 的值放在最后面（默认启用）
+ * [nf]     把 name= 的值放在最前面
  *
  *** 保留参数
  * [blkey=iplc+gpt+NF+IPLC] 用+号添加多个关键词 保留节点名的自定义字段 需要区分大小写!
  * 如果需要修改 保留的关键词 替换成别的 可以用 > 分割 例如 [#blkey=GPT>新名字+其他关键词] 这将把【GPT】替换成【新名字】
- * 例如      https://raw.githubusercontent.com/Keywos/rule/main/rename.js#flag&blkey=GPT>新名字+NF
+ * 例如     https://raw.githubusercontent.com/llxo/proxycr/refs/heads/main/substore/rename.js#flag&blkey=GPT>新名字+NF
  * [blgd]   保留: 家宽 IPLC ˣ² 等
  * [bl]     正则匹配保留 [0.1x, x0.2, 6x ,3倍]等标识
  * [nx]     保留1倍率与不显示倍率的
